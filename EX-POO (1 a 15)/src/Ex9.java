4b838d2791e24a6842fb46c082291b77f3f0ff3a
Enzo Guedes <enzoguedes@MacBook-Air-de-Enzo.local>
2025-03-20 21:17:24 -0300

    Ex Fixacao

--- EX-POO (1 a 15)/src/Ex9.java
+++ EX-POO (1 a 15)/src/Ex9.java
@@ -4,10 +4,12 @@ ou para menos. Se a parte decimal for menor que 0.5 arredonda para
 menos. Se for maior ou igual a 0.5 arredonda para mais.
 */
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Ex9 {
     public static void main(String [] args) {
+        /*
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Digite um número real: ");
@@ -23,10 +25,22 @@ public class Ex9 {
             arredondado = inteiro + 1;
         }
 
-
         System.out.println("Número arredondado: " + arredondado);
 
+         */
 
+        double x = Double.parseDouble(JOptionPane.showInputDialog("Digite um numero real"));
+        int inteiro = (int) x;
+        double decimal = x - inteiro;
+
+        int arredondado;
+        if (decimal < 0.5){
+            arredondado = inteiro;
+        }
+        else {
+            arredondado = inteiro + 1;
+        }
+        JOptionPane.showMessageDialog(null, "Número arredondado " + arredondado);
         }
 
     }
