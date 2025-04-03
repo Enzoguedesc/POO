695a2ae6ba763a634320806ba0cdd69d13ceb031
Enzoguedesc <enzoguedesc@gmail.com>
2025-04-02 23:57:03 -0300

    praticando

--- ClassesObjetos/src/Programa.java
+++ ClassesObjetos/src/Programa.java
@@ -6,14 +6,18 @@ public class Programa {
         Aluno aluno1 = new Aluno("01", "Enzo", "CDIA");
         Aluno aluno2 = new Aluno("02", "Bruna", "ADM");
 
-        JOptionPane.showMessageDialog(null, aluno2.curso);
+        System.out.println(aluno2.curso);                       // imprimir o Curso do aluno2 = ADM
+        //JOptionPane.showMessageDialog(null, aluno2.curso);
 
-        aluno2.trocaDeCurso("ES");
-        JOptionPane.showMessageDialog(null, aluno2.curso);
+        aluno2.trocaDeCurso("ES");                   // Ao invez de imprimir AMD, agora vai imprimir ES
+        System.out.println(aluno2.curso);
+        //JOptionPane.showMessageDialog(null, aluno2.curso);
 
-        JOptionPane.showMessageDialog(null, aluno1.nome);
+        System.out.println(aluno1.nome);                        // imprimir o novo do aluno1 = Enzo
+        //JOptionPane.showMessageDialog(null, aluno1.nome);
 
-        aluno1.trocarDeNome("Gabriel");
-        JOptionPane.showMessageDialog(null, aluno1.nome);
+        aluno1.trocarDeNome("Gabriel");               // Ao invez de imprimir Enzo, agora vai imprimir Gabriel
+        System.out.println(aluno1.nome);
+        //JOptionPane.showMessageDialog(null, aluno1.nome);
     }
 }
--- entrada/src/Programa.java
+++ entrada/src/Programa.java
@@ -1,3 +1,4 @@
+/*
 import javax.swing.*;
 
 public class Programa {
@@ -10,13 +11,35 @@ public class Programa {
 
         if(idadeDefinitiva >= 18){
             System.out.println("Você está habilitado a se cadastrar no curso");
-            JOptionPane.showMessageDialog(null, "Você está habilitado a se cadastrar no curso!");
+            //JOptionPane.showMessageDialog(null, "Você está habilitado a se cadastrar no curso!");
         }
         else {
-            JOptionPane.showMessageDialog(null, "Você não poderá se cadastrar no curso :( ");
+            //JOptionPane.showMessageDialog(null, "Você não poderá se cadastrar no curso :( ");
         }
 
         System.out.println(nome);
         System.out.println(idadeDefinitiva);
+    }
+}
+*/
+
+import java.util.Scanner;
+
+public class Programa{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = teclado.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = teclado.nextInt();
+
+        if (idade >= 18){
+            System.out.println("Você esta habilitado a dirigir.");
+        }
+        else{
+            System.out.println("Você não esta habilitado :( ");
+        }
     }
 }
